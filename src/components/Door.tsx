df923f82754571054dcae847a6f341c098557b57
Gabriela Ceranto <gabiceranto@gmail.com>
2023-07-22 16:26:01 -0300

    renderDoor with onchange functions

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -2,19 +2,35 @@ import styles from "./../styles/Door.module.css"
 import DoorModel from "../model/door";
 
 interface DoorProps{
-    door:DoorModel
+    value:DoorModel
+    onChange: (newDoor: DoorModel) => void
+    
 }
 
 export default function Door(props:DoorProps) {
-    const {door} = props
+    const door = props.value
   const select = door.select ? styles.select : ''
-  return (
-    <div className={styles.area}>
-      <div className={`${styles.frame} ${select}`}>
-        <div className={styles.door}>
+
+  const changeSelection = e => props.onChange(door.changeSelection());
+  const open = e => {
+    e.stopPropagation()
+    props.onChange(door.toOpen());
+  }
+
+  function renderDoor(){
+    return(
+      <div className={styles.door}>
           <div className={styles.number}>{door.number}</div>
-          <div className={styles.door_handle}></div>
+          <div className={styles.door_handle} onClick={open}></div>
         </div>
+    )
+  }
+  
+
+  return (
+    <div className={styles.area} onClick={changeSelection}>
+      <div className={`${styles.frame} ${select}`}>
+      {door.open ? false : renderDoor()}
       </div>
       <div className={styles.floor}></div>
     </div>
